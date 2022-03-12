cac3e19681f07182ad54ba71ac6bcd62546fc913
syunduel <5934746+syunduel@users.noreply.github.com>
2022-03-12 11:45:32 +0900

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -1,10 +1,10 @@
-# reveal.sh
+# reflesh.sh
 URL_HEAD="https://testnets-api.opensea.io/api/v1/asset/0x90a3fba08fe9066ccbb4cab3e94301df7a4312c4/"; #接続先
 URL_TAIL="/?force_update=true"
 HEAD="Content-Type:text/xml"; #リクエストヘッダー
 DIR="./out/"; #結果保存先ディレクトリ
 
-for i in {26..100} # revealしたいidの範囲
+for i in {120..200} # refleshしたいidの範囲
 do
     SETDT=`date "+%Y%m%d%H%M%S"`
     echo ${URL_HEAD}${i}${URL_TAIL}
