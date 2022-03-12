756d5fbd17f68f631176ce62aca1dbc31949d40a
syunduel <5934746+syunduel@users.noreply.github.com>
2022-03-12 11:33:51 +0900

    Add files via upload

---
+++ refresh.sh
@@ -0,0 +1,13 @@
+# reveal.sh
+URL_HEAD="https://testnets-api.opensea.io/api/v1/asset/0x90a3fba08fe9066ccbb4cab3e94301df7a4312c4/"; #接続先
+URL_TAIL="/?force_update=true"
+HEAD="Content-Type:text/xml"; #リクエストヘッダー
+DIR="./out/"; #結果保存先ディレクトリ
+
+for i in {26..100} # revealしたいidの範囲
+do
+    SETDT=`date "+%Y%m%d%H%M%S"`
+    echo ${URL_HEAD}${i}${URL_TAIL}
+    curl -H ${HEAD} ${URL_HEAD}${i}${URL_TAIL} > $DIR/$SETDT.txt;
+    sleep 1;
+done
