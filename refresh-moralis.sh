8f6e8cd74860e01b98321c3acc7cd4fb395fbfcb
shunichiro <5934746+syunduel@users.noreply.github.com>
2022-08-14 19:22:17 +0900

    remove echo APIKEY

--- refresh-moralis.sh
+++ refresh-moralis.sh
@@ -1,6 +1,5 @@
 ADDRESS="0xdd510ce28dfd085d9cf58f5402ca6d63985e83c0";
 APIKEY=$1;
-echo ${APIKEY};
 
 URL_HEAD="https://deep-index.moralis.io/api/v2/nft/"
 URL_TAIL="/metadata/resync?chain=eth&flag=uri&mode=async"
